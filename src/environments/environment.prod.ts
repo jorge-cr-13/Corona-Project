ceaaddf9625d45763db2346ad2527f0d7a01f6a8
jorge-cr-13 <jogamboa4@gmail.com>
2020-12-18 13:13:41 -0600

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAR3qR9gLCICCYgNADmVW05sQlq1DxlkMQ",
+    authDomain: "corona-data-follow.firebaseapp.com",
+    databaseURL: "https://corona-data-follow.firebaseio.com",
+    projectId: "corona-data-follow",
+    storageBucket: "corona-data-follow.appspot.com",
+    messagingSenderId: "935947220656",
+    appId: "1:935947220656:web:4e52412364c8e497b4ed75",
+    measurementId: "G-T9DTL7TZ55"
+  }
 };
